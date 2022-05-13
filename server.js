78b70c54dbbae4c9b6494a8c201b06da4640906d
djteiji <djteiji@gmail.com>
2022-05-13 03:23:47 -0700

    got front end working

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 const { notes } = require('./db/db');
 const fs = require('fs');
 const path = require('path');
@@ -52,6 +54,14 @@ app.post('/api/notes', (req, res) => {
     const note = createNote(req.body, notes);
     res.json(note);
     }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
 app.listen(3001, () => {
