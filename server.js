f45df9fc3f86a6f3b672f9b5aa80c91efda62078
djteiji <djteiji@gmail.com>
2022-05-14 01:30:31 -0700

    add port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // parse incoming string or array data
@@ -80,6 +81,6 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
